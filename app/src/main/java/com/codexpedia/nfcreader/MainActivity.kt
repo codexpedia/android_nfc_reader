65196381aadab3237f068a3b3ea6ebd13de2aedd
codexpedia <codexpedia.com@gmail.com>
2022-03-09 22:07:37 -0500

    initial commit

---
+++ app/src/main/java/com/codexpedia/nfcreader/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.codexpedia.nfcreader
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.codexpedia.nfcreader.databinding.ActivityMainBinding
+import java.io.UnsupportedEncodingException
+import java.nio.charset.Charset
+import kotlin.experimental.and
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tvNFCContent: TextView
+    private lateinit var nfcAdapter: NfcAdapter
+    private lateinit var pendingIntent: PendingIntent
+    var myTag: Tag? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tvNFCContent = binding.nfcContents
+        tvNFCContent.text = "Place the back of the phone over a NFC tag to read message from NFC tag"
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show()
+            finish()
+        }
+
+        readFromIntent(intent)
+
+        pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            0
+        )
+        val tagDetected = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        readFromIntent(intent)
+    }
+
+    private fun readFromIntent(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == action || NfcAdapter.ACTION_TECH_DISCOVERED == action || NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            myTag = intent.getParcelableExtra<Parcelable>(NfcAdapter.EXTRA_TAG) as Tag?
+            val rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            var msgs = mutableListOf<NdefMessage>()
+            if (rawMsgs != null) {
+                for (i in rawMsgs.indices) {
+                    msgs.add(i, rawMsgs[i] as NdefMessage)
+                }
+                buildTagViews(msgs.toTypedArray())
+            }
+        }
+    }
+
+    private fun buildTagViews(msgs: Array<NdefMessage>) {
+        if (msgs == null || msgs.isEmpty()) return
+        var text = ""
+        val payload = msgs[0].records[0].payload
+        val textEncoding: Charset = if ((payload[0] and 128.toByte()).toInt() == 0) Charsets.UTF_8 else Charsets.UTF_16 // Get the Text Encoding
+        val languageCodeLength: Int = (payload[0] and 51).toInt() // Get the Language Code, e.g. "en"
+        try {
+            // Get the Text
+            text = String(
+                payload,
+                languageCodeLength + 1,
+                payload.size - languageCodeLength - 1,
+                textEncoding
+            )
+        } catch (e: UnsupportedEncodingException) {
+            Log.e("UnsupportedEncoding", e.toString())
+        }
+        tvNFCContent.text = "Message read from NFC Tag:\n $text"
+    }
+}
